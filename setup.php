302f94f8ea1ea920d40cdf847fea1684fb5a5e27
tsmr <thetsmr@gmail.com>
2022-01-29 10:19:36 +0100

    Prepare rc2

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_CMDB_VERSION', '3.0.0-rc1');
+define('PLUGIN_CMDB_VERSION', '3.0.0-rc2');
 
 if (!defined("PLUGIN_CMDB_DIR")) {
    define("PLUGIN_CMDB_DIR", Plugin::getPhpDir("cmdb"));
