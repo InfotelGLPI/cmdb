14a6ad0fc8e8c4cb094da5a4bd1bd861413d0ede
tsmr <thetsmr@gmail.com>
2022-02-03 18:13:30 +0100

    Fix install

--- install/sql/update-3.0.0.sql
+++ install/sql/update-3.0.0.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `glpi_plugin_cmdb_operationprocesses` ADD `users_id` int(11) NOT NULL default '0' COMMENT 'RELATION to glpi_users (id)';
+ALTER TABLE `glpi_plugin_cmdb_operationprocesses` ADD `users_id` int unsigned NOT NULL default '0' COMMENT 'RELATION to glpi_users (id)';
