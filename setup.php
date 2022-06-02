43dd62928b9650e2730f163642a616ba2b9175b6
tsmr <thetsmr@gmail.com>
2022-06-02 08:30:32 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_CMDB_VERSION', '3.0.1');
+define('PLUGIN_CMDB_VERSION', '3.0.2');
 
 if (!defined("PLUGIN_CMDB_DIR")) {
    define("PLUGIN_CMDB_DIR", Plugin::getPhpDir("cmdb"));
