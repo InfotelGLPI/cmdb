557eb7af089f29af58dba1d6001672e3cd1b2314
Thetsmr <thetsmr@gmail.com>
2026-05-26 09:53:20 +0200

    Update HEADERS

--- public/js/show_criticity.js.php
+++ public/js/show_criticity.js.php
@@ -1,4 +1,32 @@
 <?php
+
+/*
+ -------------------------------------------------------------------------
+ cmdb plugin for GLPI
+ Copyright (C) 2020-2026 by the cmdb Development Team.
+
+ https://github.com/InfotelGLPI/cmdb
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of cmdb.
+
+ cmdb is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ cmdb is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with cmdb. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
 global $CFG_GLPI;
 
 use GlpiPlugin\Cmdb\Criticity_Item;
