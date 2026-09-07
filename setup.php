8bcece26eee020f0dd8c151d075b51369c1571a5
Thetsmr <thetsmr@gmail.com>
2026-09-07 09:34:18 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -42,7 +42,7 @@ use GlpiPlugin\Cmdb\Profile;
 
 use function Safe\mkdir;
 
-define('PLUGIN_CMDB_VERSION', '3.1.9');
+define('PLUGIN_CMDB_VERSION', '3.1.10');
 
 if (!defined("PLUGIN_CMDB_DIR")) {
     define("PLUGIN_CMDB_DIR", Plugin::getPhpDir("cmdb"));
